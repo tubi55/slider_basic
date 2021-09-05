e1ffcd3f1165a8872363505df5b9a95c23666484
tubi55 <tubi55@nate.com>
2021-09-05 14:53:33 +0900

    활성화버튼 재클릭 방지

--- js/mySlider.js
+++ js/mySlider.js
@@ -1,7 +1,13 @@
 /*
+    addClass() : 클래스명 추가
+    removeClass() : 클래스명 제거
+    toggleClass() : 클래스가 없으면 추가, 있으면 제거
+    hasClass() : 클래스명이 있으면 true반환, 없으면 false반환
+
     1.코드안에 상수값은 모두 전역변수 처리
     2.이벤트와 기능을 분리 (이벤트문+함수)
     3.이미 활성화된 버튼에 재이벤트 방지
+    4.모션중 재이벤트 방지 (다음주)
 */
 var btns = $(".btns li");
 var panel = $(".panel");
@@ -9,11 +15,14 @@ var speed = 1000;
 
 btns.on("click",function(e){
     e.preventDefault();
+
+    var isOn = $(this).hasClass("on");   
+    if(isOn) return;    
     activation(this);
 });
 
-function activation(el){
-    console.log("you called me again!!!");
+function activation(el){  
+    console.log("called!!!");
     var i = $(el).index();
     panel.animate({marginLeft: -100*i+"%"},speed);
     
